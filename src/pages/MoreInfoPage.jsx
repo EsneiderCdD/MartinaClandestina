4c258482363519335cdc126cd7e678c890b1d030
EsneiderCdD <ecd.97@hotmail.com>
2025-07-08 20:51:49 -0500

    Flex Imagenes

--- src/pages/MoreInfoPage.jsx
+++ src/pages/MoreInfoPage.jsx
@@ -1,19 +1,35 @@
-import styles from '../components/DayMenu/DayMenu.module.css'
-
+import styles from './MoreInfoPage.module.css'
+import MC1 from '../assets/images/MC1.webp';
+import MC4 from '../assets/images/MC4.webp';
+import MC3 from '../assets/images/MC3.webp';
+import MC5 from '../assets/images/MC5.webp';
+import MC6 from '../assets/images/MC6.webp';
+import MC7 from '../assets/images/MC7.webp';
+import MC8 from '../assets/images/MC8.webp';
+import MC9 from '../assets/images/MC9.webp';
 const MoreInfoPage = () => {
   return (
     <div className={styles.contenedor}>
       <h2 className={styles.titulo}>Conocer Más</h2>
 
       <div className={styles.bloque}>
-        <h3 className={styles.subtitulo}>Nuestros Ingredientes</h3>
-        <p>Seleccionamos cuidadosamente cada ingrediente para garantizar frescura y calidad.</p>
+        <img className={styles.imagen} src={MC1} alt="Imagen 1" />
+        <img className={styles.imagen} src={MC4} alt="Imagen 2" />
+      </div>
 
-        <h3 className={styles.subtitulo}>Procesos de Cocina</h3>
-        <p>Nuestros procesos siguen estándares de seguridad e higiene, asegurando platos deliciosos y saludables.</p>
+      <div className={styles.bloque}>
+        <img className={styles.imagen} src={MC3} alt="Imagen 3" />
+        <img className={styles.imagen} src={MC5} alt="Imagen 4" />
+      </div>
 
-        <h3 className={styles.subtitulo}>Compromiso con el Cliente</h3>
-        <p>Nos esforzamos cada día para mejorar nuestro servicio y superar las expectativas de nuestros clientes.</p>
+      <div className={styles.bloque}>
+        <img className={styles.imagen} src={MC6} alt="Imagen 5" />
+        <img className={styles.imagen} src={MC7} alt="Imagen 6" />
+      </div>
+
+      <div className={styles.bloque}>
+        <img className={styles.imagen} src={MC8} alt="Imagen 7" />
+        <img className={styles.imagen} src={MC9} alt="Imagen 8" />
       </div>
     </div>
   )
