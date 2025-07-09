1465ef6fa9b2eae2640f6ca98e7f4e2f6091882e
EsneiderCdD <ecd.97@hotmail.com>
2025-07-08 23:21:20 -0500

    Order Style

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,39 +1,39 @@
 export const menus = {
   lunes: {
-    proteinas: ['Filete de pollo a la toscana', 'Filete de cerdo al ajillo'],
-    bases: ['Arroz blanco', 'Arroz con verduras'],
-    acompañamientos: ['Puré de plátano maduro', 'Papa amarilla al limón'],
-    ensaladas: ['Mix de lechuga', 'Repollo morado, zuccini, zanahoria, cebolla, tomate y vinagreta artesanal'],
-    sopas: ['Sopa de fríjoles', 'Crema de brócoli'],
-    bebidas: ['limonada natural', 'Tomate de arbol']
+    Proteinas: ['Filete de pollo a la toscana', 'Filete de cerdo al ajillo'],
+    Bases: ['Arroz blanco', 'Arroz con verduras'],
+    Acompañamientos: ['Puré de plátano maduro', 'Papa amarilla al limón'],
+    Ensaladas: ['Mix de lechuga', 'Repollo morado, zuccini, zanahoria, cebolla, tomate y vinagreta artesanal'],
+    Sopas: ['Sopa de fríjoles', 'Crema de brócoli'],
+    Bebidas: ['Limonada natural', 'Tomate de arbol']
   },
   martes: {
-        proteinas: ['Filete de pollo al romero', 'Filete de cerdoa la paprika cremoso'],
-    bases: ['Arroz blanco', 'Arroz con zanahoria'],
-    acompañamientos: ['Maduro al vapor', 'Suffle de brócoli'],
-    ensaladas: ['Mix de lechuga', 'Repollo morado, zucchini,tomate, pepino, cebolla, aguacate y vinagreta artesanal'],
-    sopas: ['Sopa de fríjoles', 'Crema de guineo'],
-    bebidas: ['limonada natural', 'Maracuya']
+    Proteinas: ['Filete de pollo al romero', 'Filete de cerdoa la paprika cremoso'],
+    Bases: ['Arroz blanco', 'Arroz con zanahoria'],
+    Acompañamientos: ['Maduro al vapor', 'Suffle de brócoli'],
+    Ensaladas: ['Mix de lechuga', 'Repollo morado, zucchini,tomate, pepino, cebolla, aguacate y vinagreta artesanal'],
+    Sopas: ['Sopa de fríjoles', 'Crema de guineo'],
+    Bebidas: ['Limonada natural', 'Maracuya']
   },
   miercoles: {
-      proteinas: ['Filete de pollo a la toscana', 'Filete de cerdo al ajillo'],
-    bases: ['Arroz blanco', 'Arroz con verduras'],
-    acompañamientos: ['Puré de plátano maduro', 'Papa amarilla al limón'],
-    ensaladas: ['Mix de lechuga', 'Repollo morado, zuccini, zanahoria, cebolla, tomate y vinagreta artesanal'],
-    sopas: ['Sopa de fríjoles', 'Crema de brócoli'],
-    bebidas: ['limonada natural', 'Tomate de arbol']
+    Proteinas: ['Filete de pollo a la toscana', 'Filete de cerdo al ajillo'],
+    Bases: ['Arroz blanco', 'Arroz con verduras'],
+    Acompañamientos: ['Puré de plátano maduro', 'Papa amarilla al limón'],
+    Ensaladas: ['Mix de lechuga', 'Repollo morado, zuccini, zanahoria, cebolla, tomate y vinagreta artesanal'],
+    Sopas: ['Sopa de fríjoles', 'Crema de brócoli'],
+    Bebidas: ['Limonada natural', 'Tomate de arbol']
   },
   jueves: {
-      proteinas: ['Posta sudada', 'Filete de pollo en salsa de naranja'],
-    bases: ['Arroz blanco', 'Arroz de fideos'],
-    acompañamientos: ['Puré de ahuyama', 'Tortillas de espinaca'],
-    ensaladas: ['Mix de lechuga', 'Zanahoria, fresa, tomate cherry, mango y vinagreta artesanal'],
-    sopas: ['Sopa de fríjoles', 'Crema de maíz'],
-    bebidas: ['limonada natural', 'Guayaba']
+    Proteinas: ['Posta sudada', 'Filete de pollo en salsa de naranja'],
+    Bases: ['Arroz blanco', 'Arroz de fideos'],
+    Acompañamientos: ['Puré de ahuyama', 'Tortillas de espinaca'],
+    Ensaladas: ['Mix de lechuga', 'Zanahoria, fresa, tomate cherry, mango y vinagreta artesanal'],
+    Sopas: ['Sopa de fríjoles', 'Crema de maíz'],
+    Bebidas: ['Limonada natural', 'Guayaba']
   },
   viernes: {
-    proteina: ['Pescado', 'Carne'],
-    base: ['Arroz con coco', 'Yuca'],
+    Proteina: ['Pescado', 'Carne'],
+    BarPropase: ['Arroz con coco', 'Yuca'],
     entrada: ['Fruta', 'Ensalada']
   }
 }
